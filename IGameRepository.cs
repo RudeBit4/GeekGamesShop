13b8fa620856a01d13d32cc803359e7b64d4c7e3
MarchukOlexander <45167233+MarchukOlexander@users.noreply.github.com>
2019-03-19 13:33:51 +0200

    Add files via upload

---
+++ IGameRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeekGameStore.Domain.Entities;
+
+namespace GeekGameStore.Domain.Abstract
+{
+    public interface IGameRepository
+    {
+        IEnumerable<Game> Games { get; }
+    }
+}
