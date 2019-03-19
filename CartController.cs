ce2244009ace0fb38c2a067c9b6bc5d4fb94b4a1
victoriashining21 <48434824+victoriashining21@users.noreply.github.com>
2019-03-19 13:36:08 +0200

    Add files via upload

---
+++ CartController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeekGameStore.Domain.Abstract;
+using GeekGameStore.Domain.Entities;
+using GeekGameStore.WebUI.Models;
+
+namespace GeekGameStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IGameRepository repository;
+        public CartController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(Cart cart, string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = cart,
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(Cart cart, int gameId, string returnUrl)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                cart.AddItem(game, 1);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(Cart cart, int gameId, string returnUrl)
+        {
+            Game game = repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                cart.RemoveLine(game);
+            }
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+    }
+}
---
+++ NavController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GeekGameStore.Domain.Entities;
+using GeekGameStore.Domain.Concrete;
+using GeekGameStore.Domain.Abstract;
+
+namespace GeekGameStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IGameRepository repository;
+
+        public NavController(IGameRepository repo)
+        {
+            repository = repo;
+        }
+
+        public PartialViewResult Menu(string category = null)
+        {
+            ViewBag.SelectedCategory = category;
+
+            IEnumerable<string> categories = repository.Games
+                .Select(game => game.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
+        }
+    }
+}
