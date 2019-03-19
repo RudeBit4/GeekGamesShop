e622461f930af0a8b425070affdd59e141f6b8ec
Ilona <32190378+RudeBit4@users.noreply.github.com>
2019-03-19 13:34:25 +0200

    Add files via upload

---
+++ EFDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeekGameStore.Domain.Entities;
+using System.Data.Entity;
+
+namespace GeekGameStore.Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+    }
+}
---
+++ EFGameRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeekGameStore.Domain.Entities;
+using GeekGameStore.Domain.Abstract;
+
+namespace GeekGameStore.Domain.Concrete
+{
+    public class EFGameRepository : IGameRepository
+    {
+        EFDbContext context = new EFDbContext();
+
+        public IEnumerable<Game> Games
+        {
+            get { return context.Games; }
+        }
+    }
+}
