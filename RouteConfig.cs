bb76810947150ac79b9a6de3ec351cd437884387
Ilona <32190378+RudeBit4@users.noreply.github.com>
2019-03-19 13:57:49 +0200

    Add files via upload

---
+++ RouteConfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GeekGameStore.WebUI
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Game",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
+            );
+
+            routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Game", action = "List", category = (string)null },
+                constraints: new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Game", action = "List", page = 1 }
+            );
+
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Game", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
+        }
+    }
+}
